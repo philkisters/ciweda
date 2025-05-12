b0f219e46cf23feaab81233549c34fc468160fe4
Philipp Kisters <philkisters@gmail.com>
2025-05-12 14:55:35 +0200

    added additional measurement types from dwd

--- src/main/java/me/kisters/ciweda/db/entities/MeasurementType.java
+++ src/main/java/me/kisters/ciweda/db/entities/MeasurementType.java
@@ -10,16 +10,29 @@ public enum MeasurementType {
     GUST_ANGLE,
     RAIN_60MIN,
     RAIN_24H,
-    RAIN_LIVE;
+    RAIN_LIVE,
+    PRESSURE_24H,
+    TEMPERATURE_24H,
+    HUMIDITY_24H,
+    WIND_STRENGTH_24H,
+    GUST_STRENGTH_24H_MAX,
+    CLOUD_COVERAGE_24H,
+    PRECIPITATION_TYPE,
+    SUN_24H,
+    SNOW_24H;
+
 
     public static String getUnitForType(MeasurementType type) {
         return switch (type) {
-            case PRESSURE -> "mBar";
-            case TEMPERATURE -> "Celsius";
-            case HUMIDITY -> "percentage";
-            case WIND_STRENGTH, GUST_STRENGTH -> "km/h";
+            case PRESSURE, PRESSURE_24H -> "mBar";
+            case TEMPERATURE, TEMPERATURE_24H -> "Celsius";
+            case HUMIDITY, HUMIDITY_24H -> "percentage";
+            case WIND_STRENGTH, GUST_STRENGTH, WIND_STRENGTH_24H, GUST_STRENGTH_24H_MAX -> "km/h";
             case WIND_ANGLE, GUST_ANGLE -> "degrees";
-            case RAIN_60MIN, RAIN_24H, RAIN_LIVE -> "mm";
+            case RAIN_60MIN, RAIN_24H, RAIN_LIVE, SNOW_24H -> "mm";
+            case CLOUD_COVERAGE_24H -> "eights";
+            case PRECIPITATION_TYPE -> "code";
+            case SUN_24H -> "hours";
             default -> "Unknown";
         };
     }
